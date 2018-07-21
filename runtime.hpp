f70ae8a9281f86ac38c7d1880822efc47d8982b8
survari <tristanpieper080803@gmail.com>
2018-07-21 15:45:17 +0200

    Added runtime class.

---
+++ runtime.hpp
@@ -0,0 +1,16 @@
+#ifndef RUNTIME_HPP
+#define RUNTIME_HPP
+
+#include <vector>
+
+#include "package.hpp"
+
+class Runtime
+{
+public:
+    std::vector<Package> to_install;
+    std::vector<Package> to_remove;
+    std::vector<Package> to_update;
+} Runtime;
+
+#endif
