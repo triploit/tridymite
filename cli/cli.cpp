78f266e928c8b994959e2508534f97e9910ac52b
survari <tristanpieper080803@gmail.com>
2018-07-26 23:27:39 +0200

    Moved CLI-Class.

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -4,6 +4,21 @@
 
 #include "cli.hpp"
 
+void CLI::installPackage(Package p)
+{
+
+}
+
+void CLI::removePackage(Package p)
+{
+
+}
+
+void CLI::updatePackage(Package p)
+{
+
+}
+
 void CLI::parseArguments(std::vector<std::string> args)
 {
     for (int i = 0; i < args.size(); i++)
--- cli/cli.hpp
+++ cli/cli.hpp
@@ -2,10 +2,14 @@
 #define CLI_HPP
 
 #include <vector>
+#include "../package.hpp"
 
 class CLI 
 {
 public:
+    void installPackage(Package package);
+    void removePackage(Package package);
+    void updatePackage(Package package);
     void parseArguments(std::vector<std::string> arguments);
     
     CLI() {}
