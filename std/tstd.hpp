ea7bc63d53662b3f8557d0396ab136f8cdb2fdde
survari <tristanpieper080803@gmail.com>
2018-11-26 20:52:14 +0100

    Added family and prefix methods for arguments.

--- std/tstd.hpp
+++ std/tstd.hpp
@@ -10,7 +10,7 @@
 
 namespace tstd
 {
-    std::vector<std::string> split(std::string s, char delim)
+    std::vector<std::string> split(std::string s, char delim) // split a string
     {
         std::vector<std::string> result;
         std::string tmp = "";
@@ -30,6 +30,56 @@ namespace tstd
             result.push_back(tmp);
 
         return result;
+    }
+
+    std::string add_prefix(std::string arg) // add a prefix (- or --) to argument (like "help" -> "--help")
+    {
+        if (arg.size() > 2)
+            return "--"+arg;
+        return "-"+arg;
+    }
+
+    std::vector<std::string> get_family(std::string arg, std::vector<std::string> arg_name)
+    {
+        std::vector<std::string> family;
+
+        for (int x = 0; x < arg_name.size(); x++)
+        {
+            std::string tmp;
+
+            for (char c : arg_name[x])
+            {
+                if (c == ',')
+                {
+                    family.push_back(tmp);
+                    tmp = "";
+                    continue;
+                }
+
+                tmp += c;
+            }
+
+            if (!tmp.empty())
+            {
+                family.push_back(tmp);
+                tmp = "";
+            }
+
+            bool found = false;
+
+            for (std::string s : family)
+            {
+                if (add_prefix(s) == arg)
+                    found = true;
+            }
+
+            if (found)
+                return family;
+
+            family.clear();
+        }
+
+        return family;
     }
 };
 
