975fc633d04ceef67a6a7ad6e937f50033663dc2
survari <tristanpieper080803@gmail.com>
2018-12-16 12:15:42 +0100

    Added functions for cleaning temporary files.

--- runtime.hpp
+++ runtime.hpp
@@ -2,16 +2,83 @@
 #define RUNTIME_HPP
 
 #include <vector>
+#include <cstdio>
+#include <sys/stat.h>
+#include <cstring>
+#include <unistd.h>
 
 #include "package.hpp"
 
 class Runtime
 {
 public:
-    std::vector<Package> to_install;
-    std::vector<Package> to_remove;
-    std::vector<Package> to_update;
+    inline static std::vector<Package> to_install;
+    inline static std::vector<Package> to_remove;
+    inline static std::vector<Package> to_update;
 
-} Runtime;
+    inline static std::vector<std::string> files_to_clean;
+    inline static std::vector<std::string> directories_to_clean;
+
+    inline static std::string tmp_dir;
+
+    static void init()
+    {
+        tmp_dir = std::string("/tmp/tridy-"+std::to_string(getpid()));
+
+        if (mkdir(tmp_dir.c_str(), 0777) == -1)
+        {
+            std::cout << "error: coudln't create tmp-directory: " << strerror(errno) << std::endl;
+            Runtime::exit(1);
+        }
+
+        Runtime::directories_to_clean.push_back(tmp_dir);
+    }
+
+    static bool cleanFiles()
+    {
+        bool success = true;
+
+        for (const std::string &file : Runtime::files_to_clean)
+        {
+            if(std::remove(file.c_str()) != 0)
+            {
+                std::cout << "error: couldn't delete file: " << file << std::endl;
+                success = false;
+            }
+        }
+
+        return success;
+    }
+
+    static bool clearDirectories()
+    {
+        bool success = true;
+
+        for (const std::string &file : Runtime::directories_to_clean)
+        {
+            if(rmdir(file.c_str()) != 0)
+            {
+                std::cout << "error: couldn't delete directory: " << file << std::endl;
+                success = false;
+            }
+        }
+
+        return success;
+    }
+
+    static void exit(int i)
+    {
+        if (!Runtime::cleanFiles() ||
+            !Runtime::clearDirectories())
+        {
+            std::cout << "error: error at cleaning up temporary files or directories." << std::endl;
+        }
+
+        if (i != 0)
+            std::cout << "exiting with code " << i << std::endl;
+
+        std::exit(i);
+    }
+};
 
 #endif
