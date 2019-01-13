81d201343924168d9dc7db207611455246e0e756
Survari <tristanpieper080803@gmail.com>
2019-01-13 20:27:34 +0100

    Added simple fallback.

--- translation/translation.cpp
+++ translation/translation.cpp
@@ -46,7 +46,76 @@ std::string Translation::get(const std::string &s, const bool &nl, const bool &b
 void Translation::loadConfig(std::string path)
 {
     fallback = YAML::Load(R"V0G0N(
-error: true
+# cli
+
+cli.argument.not_found: "error: argument \"%s\" not found!"
+cli.argument.no_passed: "error: no argument passed!"
+cli.argument.too_many: "error: too many parameters for argument \"%s\""
+cli.argument.too_few: "error: too few parameters for argument \"%s\""
+cli.version: "version"
+
+# manager/dependencies
+
+# manager/install
+
+# manager/packages
+
+# manager/remove
+
+# script
+
+script.parse_file.file_not_found: "error: can't go on without a file."
+script.parse_file.to_many_brackets: "error: line %d: to many \"}\" in file %s!"
+script.parse_file.unfinished_declaration: "error: line %d: unfinished declaration of function in build script."
+
+# std
+
+arguments.help.help: "shows this help page"
+arguments.help.install: "install a package"
+arguments.help.remove: "remove a package"
+arguments.help.update: "update a package"
+arguments.help.update_all: "update all installed packages"
+arguments.help.local: "installing a package from the source in the given directory"
+arguments.help.verbose: "show extended (verbose) output"
+arguments.help.insecure: "installing packages without asking security questions"
+arguments.help.server: "setting the server for all package arguments"
+arguments.help.user: "setting the user for all package arguments"
+arguments.help.version: "show the version of tridymite"
+arguments.help.language_file: "loading all messages from the given language file"
+arguments.help.list_packages: "list all installed packages"
+arguments.help.packages: "get information of package"
+arguments.help.description: "shows description of a package"
+arguments.help.testing: "testing stuff"
+
+arguments.usage.help: ""
+arguments.usage.install: "<package> ..."
+arguments.usage.remove: "<package> ..."
+arguments.usage.update: "<package> ..."
+arguments.usage.update_all: ""
+arguments.usage.local: "<path>"
+arguments.usage.verbose: ""
+arguments.usage.insecure: ""
+arguments.usage.server: "<server>"
+arguments.usage.user: "<username>"
+arguments.usage.version: ""
+arguments.usage.language_file: "<file>"
+arguments.usage.list_packages: ""
+arguments.usage.packages: "<package>"
+arguments.usage.description: "<package>"
+arguments.usage.testing: "???"
+
+arguments.package: "<package> = <git-user>:<git-repo>[@<git-server>] (git-server is github.com in default)"
+
+# runtime
+
+runtime.not_installed: "error: it seems tridymite isn't installed yet.\nIf you have the source code here, try running the install.sh script as root."
+runtime.tmp_permission_error: "error: couldn't create tmp-directory!: "
+runtime.clean_files.error: "error: couldn't delete file: %s"
+runtime.clean_directories.error: "error: couldn't delete directory: %s"
+
+runtime.clear_up_all_tmps: "error: error at cleaning up temporary files or directories."
+runtime.exit: "exiting with code %d"
+
 )V0G0N");
 
     if (std::ifstream(path).is_open())
