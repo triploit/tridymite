8e2af051091fce80965f1471b646c3cc04e5b0c1
Survari <tristanpieper080803@gmail.com>
2019-01-13 20:24:41 +0100

    Fixed build script.

--- pkg/package.sh
+++ pkg/package.sh
@@ -1,8 +1,4 @@
 function build {
-    source pkg/functions.sh # Exported to another script file, just to show that (and how) it works :)
-    build_tridy
-}
-
-function update {
-    # Nothing to do here :)
+    cmake .
+    make
 }
