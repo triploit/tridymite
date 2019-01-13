d8c218e912b69207fdaa5752a546bec490c9b5f2
Survari <tristanpieper080803@gmail.com>
2019-01-13 20:26:21 +0100

    Added manager to remove packages.

--- manager/remove/remove_manager.hpp
+++ manager/remove/remove_manager.hpp
@@ -6,9 +6,15 @@
 #define TRIDYMITE_REMOVE_MANAGER_HPP
 
 
+#include <string>
+#include <package/package.hpp>
+
 class RemoveManager
 {
-
+public:
+    static bool unlinkProducts(const std::string &prefix, const Package &package);
+    static bool removeProducts(const std::string &prefix, const Package &package);
+    static void uninstallPackage(const Package &p);
 };
 
 
