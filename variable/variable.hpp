b51741bc408c9dff09e232f01a2a711f7d7e2ced
Survari <tristanpieper080803@gmail.com>
2019-04-18 14:45:21 +0200

    Added variable class for pretypes.

---
+++ variable/variable.hpp
@@ -0,0 +1,28 @@
+//
+// Created by survari on 16.04.19.
+//
+
+#ifndef TRIDYMITE_VARIABLE_HPP
+#define TRIDYMITE_VARIABLE_HPP
+
+
+#include <string>
+
+class Variable
+{
+private:
+    std::string name;
+    std::string value;
+
+public:
+    Variable(const std::string &name, const std::string &value);
+
+    const std::string &getName() const;
+    const std::string &getValue() const;
+
+    void setName(const std::string &name);
+    void setValue(const std::string &value);
+};
+
+
+#endif //TRIDYMITE_VARIABLE_HPP
