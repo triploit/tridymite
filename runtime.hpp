b639966f549f84d3a38d017731bdb6088c65924b
Survari <tristanpieper080803@gmail.com>
2020-07-23 17:02:22 +0200

    Changed version number to 1.1.0.

--- runtime.hpp
+++ runtime.hpp
@@ -58,7 +58,7 @@ public:
 
     inline static int pid;
     inline static YAML::Node config;
-    inline static std::string version = "1.0.7";
+    inline static std::string version = "1.1.0";
 
     static void init()
     {
