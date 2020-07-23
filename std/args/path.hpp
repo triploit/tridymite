2b0b4bb22f0ca31d23fe7a9c523cdeedd214467b
Survari <tristanpieper080803@gmail.com>
2020-07-23 17:17:57 +0200

    It just works.

--- std/args/path.hpp
+++ std/args/path.hpp
@@ -8,7 +8,18 @@ void __argument_path(const std::vector<std::string> &parameters, CLI* cli)
 {
     std::cout << Translation::get("main.installing_local_path", false) << " " << cli->getParameters("p")[0] << std::endl;
     chdir(cli->getParameters("p")[0].c_str());
-    InstallationManager::localPackage(cli->getParameters("p")[0]);
+
+    if (!InstallationManager::localPackage(cli->getParameters("p")[0]))
+    {
+        Package package = Package(YAML::LoadFile(cli->getParameters("p")[0]+"/pkg/package.yaml"));
+
+        if (package.isDependency())
+        {
+            printf(Translation::get("manager.install.cant_continue_installation_is_dep").c_str(),
+                   package.getDependantPackage().c_str());
+            Runtime::exit(1);
+        }
+    }
 }
 
 #endif //TRIDYMITE_HELP_HPP
