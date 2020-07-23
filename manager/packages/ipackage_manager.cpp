b8c8c770e7cc09245217a0bec4636745f87e968d
Survari <tristanpieper080803@gmail.com>
2020-07-23 17:12:07 +0200

    Loading local packages separated from global packages.

--- manager/packages/ipackage_manager.cpp
+++ manager/packages/ipackage_manager.cpp
@@ -5,10 +5,14 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-void IPackagesManager::load(std::string path, bool clear_all) // package = /usr/share/tridymite/conf/user+name+server.xyz/
+void IPackagesManager::load(std::string path, std::string gpath, bool clear_all) // package = /usr/share/tridymite/conf/user+name+server.xyz/
 {
     if (clear_all)
+    {
         IPackagesManager::installed_packages.clear();
+        IPackagesManager::g_installed_packages.clear();
+    }
+
     std::vector<std::string> files = tstd::read_cursive_all_files(path);
 
     for (const std::string &file : files)
@@ -22,6 +26,22 @@ void IPackagesManager::load(std::string path, bool clear_all) // package = /usr/
         {
             Package p(YAML::LoadFile(file), file);
             IPackagesManager::installed_packages.push_back(p);
+        }
+    }
+
+    files = tstd::read_cursive_all_files(gpath);
+
+    for (const std::string &file : files)
+    {
+        if (file[0] == '.')
+            continue;
+
+        if (file.size() >= 5 &&
+            (file.substr(file.size()-5, file.size()) == ".yaml" ||
+             file.substr(file.size()-4, file.size()) == ".yml"))
+        {
+            Package p(YAML::LoadFile(file), file);
+            IPackagesManager::g_installed_packages.push_back(p);
         }
     }
 }
@@ -76,4 +96,9 @@ void IPackagesManager::setInstalledPackages(const std::vector<Package> &packages
 {
     IPackagesManager::installed_packages.clear();
     IPackagesManager::installed_packages = packages;
+}
+
+const std::vector<Package> &IPackagesManager::getGloballyInstalledPackages()
+{
+    return g_installed_packages;
 }
